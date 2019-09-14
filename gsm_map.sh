041c43085c4724798ac1873f082b01337687bd94
Yuan Huang <yuanhuang100@gmail.com>
2019-09-14 19:59:25 -0400

    modify the gsm_map file

--- gsm_map.sh
+++ gsm_map.sh
@@ -1,23 +1,37 @@
 #!usr/bin/env bash
+set -e
 
-GSE=$1
-gsms=()
+mode="${MODE:-cloud}"
 
-gsm=$(esearch -db gds -query "$GSE[ACCN] AND GSM[ETYP]" | efetch -format docsum | xtract -pattern DocumentSummary -element Accession);
+if [ $mode == "cloud" ]; then
+	s3_header=$1
+	GSE=${@:2}
+elif [ $mode == "local" ]; then
+   GSE=$@	
+else
+     echo "the MODE environment varialbe has invalid value"
+     exit
+fi
 
 cd /root
 lftp -c "open ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && get SRA_Accessions.tab"
 
+cd /home
 grep ^SRR /root/SRA_Accessions.tab | grep GSM >srr_gsm.txt
-rm -f /root/SRA_Accessions.tab
 
-for a in ${gsm[@]}; do
-    grep $a srr_gsm.txt | sed 's/_r[0-9]*//' | cut -d $'\t' -f 10,11,1 >> map_batch.out;
+for g in ${GSE[@]};do
+	gsm=$(esearch -db gds -query "$GSE[ACCN] AND GSM[ETYP]" | efetch -format docsum | xtract -pattern DocumentSummary -element Accession);
+        gse_file="map_batch$g.txt"
+
+    for a in ${gsm[@]}; do
+        grep $a srr_gsm.txt | sed 's/_r[0-9]*//' | cut -d $'\t' -f 10,11,1 >> $gse_file;
+    done
+
+    if [ $mode == "cloud" ]; then
+	    aws s3 cp $gse_file $s3_header$gse_file
+	    rm -f $gse_file
+    fi
 done
 
-srr_gsm_map="map_batch.out"
-DATE=`date '+%Y%m%d'`
-folder_name="$s3_header$GSE.$DATE/"
-
-aws s3 cp map_batch.out $folder_name$srr_gsm_map 
+rm -f /root/SRA_Accessions.tab
 
